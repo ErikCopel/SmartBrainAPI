0216425a03219b496cc974c9086f7bda274c9798
erik <erik.cr42@gmail.com>
2024-11-07 18:59:33 -0300

    Setting up API call functions

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ const PORT = 3003   ;
 app.use(cors());
 app.use(express.json());
 
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
+
 app.post('/clarifai', async (req, res) => {
 
     console.log('Requisição recebida');
@@ -54,8 +75,54 @@ app.post('/clarifai', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
 
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json('error logging in');
+  }
+});
 
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.status(200).send(`success registering ${req.body.name}`);
+});
+
+app.get('/profile/:userId', (req, res) => {
+  const { userId } = req.params;
+  const user = database.users.find(user => user.id === userId);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json('user not found');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { userId } = req.body;
+  const user = database.users.find(user => user.id === userId);
+
+  if (user) {
+    user.entries++;
+    res.json(user.entries);
+  } else {
+    res.status(404).json('user not found');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
